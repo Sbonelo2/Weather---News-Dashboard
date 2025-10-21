88fdbb8cbba51586641420c9fda7913412ab42e3
Sbonelo2 <cbonelo224@gmail.com>
2025-10-21 12:35:41 +0200

    feat files & folders

---
+++ src/index.ts
@@ -0,0 +1,136 @@
+console.log("Weather and News Dashboard server running...");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const app = express();
+// const PORT = 3000;
+
+// // Serve static files (like HTML, CSS, JS)
+// app.use(express.static("public"));
+
+// // Default route
+// app.get("/", (req, res) => {
+//   res.send("🌦️ Weather & 📰 News Dashboard server is running!");
+// });
+
+// // Start the server
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
